ca1d77c1cc996f383a4bcd83b09a2c5ec2711494
Ivan Derlich <a@ivanderlich.com>
2024-10-12 21:38:53 -0300

    Initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const getPostsClickHandler = async () => {
+    console.log("Get Posts Clicked");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getPostsClickHandler}>Get Posts</button>
+      <button>Create Post</button>
     </div>
   );
 }
